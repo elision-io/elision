1190bbd384b17a64ba62d53523137d704d092100
Riddle <samuel.riddle1@gmail.com>
2022-06-06 19:52:00 -0600

    create-pair IBC packet

--- x/dex/types/packet.pb.go
+++ x/dex/types/packet.pb.go
@@ -25,6 +25,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 type DexPacketData struct {
 	// Types that are valid to be assigned to Packet:
 	//	*DexPacketData_NoData
+	//	*DexPacketData_CreatePairPacket
 	Packet isDexPacketData_Packet `protobuf_oneof:"packet"`
 }
 
@@ -70,8 +71,12 @@ type isDexPacketData_Packet interface {
 type DexPacketData_NoData struct {
 	NoData *NoData `protobuf:"bytes,1,opt,name=noData,proto3,oneof" json:"noData,omitempty"`
 }
+type DexPacketData_CreatePairPacket struct {
+	CreatePairPacket *CreatePairPacketData `protobuf:"bytes,2,opt,name=createPairPacket,proto3,oneof" json:"createPairPacket,omitempty"`
+}
 
-func (*DexPacketData_NoData) isDexPacketData_Packet() {}
+func (*DexPacketData_NoData) isDexPacketData_Packet()           {}
+func (*DexPacketData_CreatePairPacket) isDexPacketData_Packet() {}
 
 func (m *DexPacketData) GetPacket() isDexPacketData_Packet {
 	if m != nil {
@@ -87,10 +92,18 @@ func (m *DexPacketData) GetNoData() *NoData {
 	return nil
 }
 
+func (m *DexPacketData) GetCreatePairPacket() *CreatePairPacketData {
+	if x, ok := m.GetPacket().(*DexPacketData_CreatePairPacket); ok {
+		return x.CreatePairPacket
+	}
+	return nil
+}
+
 // XXX_OneofWrappers is for the internal use of the proto package.
 func (*DexPacketData) XXX_OneofWrappers() []interface{} {
 	return []interface{}{
 		(*DexPacketData_NoData)(nil),
+		(*DexPacketData_CreatePairPacket)(nil),
 	}
 }
 
@@ -130,27 +143,124 @@ func (m *NoData) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_NoData proto.InternalMessageInfo
 
+// CreatePairPacketData defines a struct for the packet payload
+type CreatePairPacketData struct {
+	SourceDenom string `protobuf:"bytes,1,opt,name=sourceDenom,proto3" json:"sourceDenom,omitempty"`
+	TargetDenom string `protobuf:"bytes,2,opt,name=targetDenom,proto3" json:"targetDenom,omitempty"`
+}
+
+func (m *CreatePairPacketData) Reset()         { *m = CreatePairPacketData{} }
+func (m *CreatePairPacketData) String() string { return proto.CompactTextString(m) }
+func (*CreatePairPacketData) ProtoMessage()    {}
+func (*CreatePairPacketData) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9e40d3eecbdb512f, []int{2}
+}
+func (m *CreatePairPacketData) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *CreatePairPacketData) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_CreatePairPacketData.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *CreatePairPacketData) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreatePairPacketData.Merge(m, src)
+}
+func (m *CreatePairPacketData) XXX_Size() int {
+	return m.Size()
+}
+func (m *CreatePairPacketData) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreatePairPacketData.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CreatePairPacketData proto.InternalMessageInfo
+
+func (m *CreatePairPacketData) GetSourceDenom() string {
+	if m != nil {
+		return m.SourceDenom
+	}
+	return ""
+}
+
+func (m *CreatePairPacketData) GetTargetDenom() string {
+	if m != nil {
+		return m.TargetDenom
+	}
+	return ""
+}
+
+// CreatePairPacketAck defines a struct for the packet acknowledgment
+type CreatePairPacketAck struct {
+}
+
+func (m *CreatePairPacketAck) Reset()         { *m = CreatePairPacketAck{} }
+func (m *CreatePairPacketAck) String() string { return proto.CompactTextString(m) }
+func (*CreatePairPacketAck) ProtoMessage()    {}
+func (*CreatePairPacketAck) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9e40d3eecbdb512f, []int{3}
+}
+func (m *CreatePairPacketAck) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *CreatePairPacketAck) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_CreatePairPacketAck.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *CreatePairPacketAck) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreatePairPacketAck.Merge(m, src)
+}
+func (m *CreatePairPacketAck) XXX_Size() int {
+	return m.Size()
+}
+func (m *CreatePairPacketAck) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreatePairPacketAck.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CreatePairPacketAck proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*DexPacketData)(nil), "elisionio.elision.dex.DexPacketData")
 	proto.RegisterType((*NoData)(nil), "elisionio.elision.dex.NoData")
+	proto.RegisterType((*CreatePairPacketData)(nil), "elisionio.elision.dex.CreatePairPacketData")
+	proto.RegisterType((*CreatePairPacketAck)(nil), "elisionio.elision.dex.CreatePairPacketAck")
 }
 
 func init() { proto.RegisterFile("dex/packet.proto", fileDescriptor_9e40d3eecbdb512f) }
 
 var fileDescriptor_9e40d3eecbdb512f = []byte{
-	// 177 bytes of a gzipped FileDescriptorProto
+	// 261 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x48, 0x49, 0xad, 0xd0,
 	0x2f, 0x48, 0x4c, 0xce, 0x4e, 0x2d, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x4d, 0xcd,
-	0xc9, 0x2c, 0xce, 0xcc, 0xcf, 0xcb, 0xcc, 0xd7, 0x83, 0xb2, 0xf4, 0x52, 0x52, 0x2b, 0x94, 0x82,
-	0xb8, 0x78, 0x5d, 0x52, 0x2b, 0x02, 0xc0, 0x2a, 0x5d, 0x12, 0x4b, 0x12, 0x85, 0xcc, 0xb9, 0xd8,
-	0xf2, 0xf2, 0x41, 0x2c, 0x09, 0x46, 0x05, 0x46, 0x0d, 0x6e, 0x23, 0x59, 0x3d, 0xac, 0x1a, 0xf5,
-	0xfc, 0xc0, 0x8a, 0x3c, 0x18, 0x82, 0xa0, 0xca, 0x9d, 0x38, 0xb8, 0xd8, 0x20, 0x16, 0x2a, 0x71,
-	0x70, 0xb1, 0x41, 0x64, 0x9d, 0x9c, 0x4f, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48, 0x8e, 0xf1, 0xc1,
-	0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21,
-	0x4a, 0x33, 0x3d, 0xb3, 0x24, 0xa3, 0x34, 0x49, 0x2f, 0x39, 0x3f, 0x57, 0x1f, 0x6a, 0xac, 0x6e,
-	0x66, 0x3e, 0x8c, 0xa9, 0x5f, 0xa1, 0x0f, 0xf2, 0x40, 0x49, 0x65, 0x41, 0x6a, 0x71, 0x12, 0x1b,
-	0xd8, 0x03, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0x4a, 0xfa, 0x65, 0x74, 0xd4, 0x00, 0x00,
-	0x00,
+	0xc9, 0x2c, 0xce, 0xcc, 0xcf, 0xcb, 0xcc, 0xd7, 0x83, 0xb2, 0xf4, 0x52, 0x52, 0x2b, 0x94, 0xd6,
+	0x32, 0x72, 0xf1, 0xba, 0xa4, 0x56, 0x04, 0x80, 0x95, 0xba, 0x24, 0x96, 0x24, 0x0a, 0x99, 0x73,
+	0xb1, 0xe5, 0xe5, 0x83, 0x58, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0xdc, 0x46, 0xb2, 0x7a, 0x58, 0x75,
+	0xea, 0xf9, 0x81, 0x15, 0x79, 0x30, 0x04, 0x41, 0x95, 0x0b, 0x45, 0x72, 0x09, 0x24, 0x17, 0xa5,
+	0x26, 0x96, 0xa4, 0x06, 0x24, 0x66, 0x16, 0x41, 0x0c, 0x94, 0x60, 0x02, 0x1b, 0xa1, 0x8d, 0xc3,
+	0x08, 0x67, 0x34, 0xe5, 0x50, 0x03, 0x31, 0x8c, 0x71, 0xe2, 0xe0, 0x62, 0x83, 0x78, 0x46, 0x89,
+	0x83, 0x8b, 0x0d, 0x62, 0xb1, 0x52, 0x14, 0x97, 0x08, 0x36, 0xfd, 0x42, 0x0a, 0x5c, 0xdc, 0xc5,
+	0xf9, 0xa5, 0x45, 0xc9, 0xa9, 0x2e, 0xa9, 0x79, 0xf9, 0xb9, 0x60, 0x4f, 0x70, 0x06, 0x21, 0x0b,
+	0x81, 0x54, 0x94, 0x24, 0x16, 0xa5, 0xa7, 0x96, 0x40, 0x54, 0x30, 0x41, 0x54, 0x20, 0x09, 0x29,
+	0x89, 0x72, 0x09, 0xa3, 0x9b, 0xed, 0x98, 0x9c, 0xed, 0xe4, 0x7c, 0xe2, 0x91, 0x1c, 0xe3, 0x85,
+	0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x4e, 0x78, 0x2c, 0xc7, 0x70, 0xe1, 0xb1, 0x1c, 0xc3,
+	0x8d, 0xc7, 0x72, 0x0c, 0x51, 0x9a, 0xe9, 0x99, 0x25, 0x19, 0xa5, 0x49, 0x7a, 0xc9, 0xf9, 0xb9,
+	0xfa, 0x50, 0x1f, 0xea, 0x66, 0xe6, 0xc3, 0x98, 0xfa, 0x15, 0xfa, 0xa0, 0xf8, 0x28, 0xa9, 0x2c,
+	0x48, 0x2d, 0x4e, 0x62, 0x03, 0xc7, 0x87, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0xb6, 0xf6, 0x14,
+	0xa3, 0xa3, 0x01, 0x00, 0x00,
 }
 
 func (m *DexPacketData) Marshal() (dAtA []byte, err error) {
@@ -206,6 +316,27 @@ func (m *DexPacketData_NoData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	}
 	return len(dAtA) - i, nil
 }
+func (m *DexPacketData_CreatePairPacket) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *DexPacketData_CreatePairPacket) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	if m.CreatePairPacket != nil {
+		{
+			size, err := m.CreatePairPacket.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintPacket(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0x12
+	}
+	return len(dAtA) - i, nil
+}
 func (m *NoData) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
@@ -222,6 +353,66 @@ func (m *NoData) MarshalTo(dAtA []byte) (int, error) {
 }
 
 func (m *NoData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *CreatePairPacketData) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *CreatePairPacketData) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *CreatePairPacketData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.TargetDenom) > 0 {
+		i -= len(m.TargetDenom)
+		copy(dAtA[i:], m.TargetDenom)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.TargetDenom)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.SourceDenom) > 0 {
+		i -= len(m.SourceDenom)
+		copy(dAtA[i:], m.SourceDenom)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.SourceDenom)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *CreatePairPacketAck) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *CreatePairPacketAck) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *CreatePairPacketAck) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	i := len(dAtA)
 	_ = i
 	var l int
@@ -264,7 +455,45 @@ func (m *DexPacketData_NoData) Size() (n int) {
 	}
 	return n
 }
+func (m *DexPacketData_CreatePairPacket) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.CreatePairPacket != nil {
+		l = m.CreatePairPacket.Size()
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	return n
+}
 func (m *NoData) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *CreatePairPacketData) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.SourceDenom)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	l = len(m.TargetDenom)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	return n
+}
+
+func (m *CreatePairPacketAck) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -343,6 +572,41 @@ func (m *DexPacketData) Unmarshal(dAtA []byte) error {
 			}
 			m.Packet = &DexPacketData_NoData{v}
 			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field CreatePairPacket", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			v := &CreatePairPacketData{}
+			if err := v.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			m.Packet = &DexPacketData_CreatePairPacket{v}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPacket(dAtA[iNdEx:])
@@ -391,6 +655,170 @@ func (m *NoData) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: NoData: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipPacket(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *CreatePairPacketData) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowPacket
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: CreatePairPacketData: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: CreatePairPacketData: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field SourceDenom", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.SourceDenom = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field TargetDenom", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.TargetDenom = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipPacket(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *CreatePairPacketAck) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowPacket
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: CreatePairPacketAck: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: CreatePairPacketAck: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
